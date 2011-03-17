fce5f1b6632a4ef0b4639423d5845d79074fa35f
Peter Horn <peter.horn@provideal.net>
2011-03-17 18:37:20 +0100

    generator stuff

--- lib/generators/tabulatr_generator.rb
+++ lib/generators/tabulatr_generator.rb
@@ -1,15 +1,15 @@
-# module Tabulatr
-#   module Generators
-#     class InstallGenerator < Rails::Generators::Base
-#       desc "Copy Tabulatr default files"
-#       source_root File.expand_path('../templates', __FILE__)
-#       class_option :template_engine
-# 
-#       def copy_initializers
-#         puts "hooray1"
-#         #copy_file 'simple_form.rb', 'config/initializers/simple_form.rb'
-#       end
-#       
-#     end
-#   end
-# end
+module Tabulatr
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      desc "Copy Tabulatr default files"
+      source_root File.expand_path('../templates', __FILE__)
+      class_option :template_engine
+
+      def copy_initializers
+        puts "hooray1"
+        #copy_file 'simple_form.rb', 'config/initializers/simple_form.rb'
+      end
+      
+    end
+  end
+end
